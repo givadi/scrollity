adee9717e5dd86a5a0ddbf98cd24ee3588994368
Aleksandra Andreeva <shu-@list.ru>
2022-11-27 19:42:36 +0300

    S-5 Кнопка загрузки картинки с ПК открывает окно

--- src/components/toolbar/components/InsertPicture.tsx
+++ src/components/toolbar/components/InsertPicture.tsx
@@ -1,12 +1,45 @@
 import styles from '../../common/barIcon/BarIcon.module.css';
 import insert_picture from '../../../assets/images/barIcons/insert_picture.svg';
+import { useState, useRef } from 'react';
 
 const InsertPicture = () => {
-  return (
-    <div className={styles.wrapper}>
-      <img className={styles.icon} src={insert_picture} alt='Insert_picture' />
-    </div>
-  );
+  const filePicker = useRef(null);
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [uploaded, setUploaded] = useState();
+
+  // const handleChange = (event) => {
+  //   console.log(event.target.files);
+  //   setSelectedFile(event.target.files[0])
+  // };
+
+
+// const handleUpload = async () => {
+//   if (!selectedFile) {
+//     alert("Please select a file");
+//     return;
+//   };
+// const formData = new FormData();
+// formData.append('file');
+// }
+
+// const onInsertPicture = (e) => {
+//  if (e.target.files[0]) {
+
+//  }
+// }
+
+
+const handlePick = () => {
+  filePicker.current.click();
 }
+
+return (
+   <div className={styles.wrapper}>
+     <img className={styles.icon} src={insert_picture} alt='Insert_picture' onClick={handlePick} />
+     <input className = {styles.hidden} type="file" ref={filePicker} accept="image/*, .png, .jpg, .jpeg"/>
+    </div>
+)
+}
+
 
 export default InsertPicture;

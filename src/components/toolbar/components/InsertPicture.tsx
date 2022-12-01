2f6fd09e2a6e07a95f59d35c884a3dcb753542dc
Aleksandra Andreeva <shu-@list.ru>
2022-12-01 20:50:36 +0300

    S-5 Изменения с практики по вставке картинки с ПК

--- src/components/toolbar/components/InsertPicture.tsx
+++ src/components/toolbar/components/InsertPicture.tsx
@@ -1,47 +1,41 @@
 import styles from '../../common/barIcon/BarIcon.module.css';
 import insert_picture from '../../../assets/images/barIcons/insert_picture.svg';
-import { useState, useRef } from 'react';
+import { useState, useRef, MutableRefObject } from 'react';
 
 const InsertPicture = () => {
-  const filePicker = useRef(null);
-  const [selectedFile, setSelectedFile] = useState(null);
+  const filePicker: MutableRefObject<HTMLInputElement | null> = useRef(null);
+  const [selectedFile, setSelectedFile] = useState(null as (File | null));
   const [uploaded, setUploaded] = useState();
+  const hostUrl = '/upload';
 
-const hostUrl = '/upload';
-
-  const handleChange = (event) => {
-    console.log(event.target.files);
-    setSelectedFile(event.target.files[0])
+  const handleChange = () => {
+    if((filePicker.current) && (filePicker.current.files)) {
+      console.log(filePicker.current.files);
+      setSelectedFile(filePicker.current.files[0])
+    }
   };
 
+  const handleUpload = async () => {
+    if (!selectedFile) {
+      alert("Please select a file");
+      return;
+    };
+    const formData = new FormData();
+    formData.append('file', selectedFile );
 
-          const handleUpload = async () => {
-            if (!selectedFile) {
-              alert("Please select a file");
-              return;
-            };
-          const formData = new FormData();
-          formData.append('file', selectedFile );
+    const res = await fetch(hostUrl, {
+      method: 'POST',
+      body: formData,
+    });
+    const data = await res.json();
 
-          const res = await fetch(hostUrl, {
-            method: 'POST',
-            body: formData,
-          });
-          const data = await res.json();
-
-setUploaded(data);
-
-          };
-
-          const onInsertPicture = (e) => {
-           if (e.target.files[0]) {
-
-           }
-          }
-
+    setUploaded(data);
+  };
 
 const handlePick = () => {
-  filePicker.current.click()
+  if (filePicker.current){
+    filePicker.current.click()
+  } 
 }
 
 return (
@@ -49,10 +43,7 @@ return (
      <img className={styles.icon} src={insert_picture} alt='Insert_picture' onClick={handlePick} />
      <input className = {styles.hidden} type="file" ref={filePicker}  onChange = {handleChange} accept="image/*, .png, .jpg, .jpeg"/>
     </div>
-
-
 )
 }
-
 
 export default InsertPicture;

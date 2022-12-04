4479f8b4a3444a84975e50bd8656f3331d7688aa
givadi <givadi@mail.ru>
2022-12-04 21:31:54 +0300

    S-6 Добавила функцию удаления слайда

--- src/actions/slides.ts
+++ src/actions/slides.ts
@@ -1,15 +1,19 @@
 import { Presentation, Slide, Selection } from '../data/types';
 import { generateId } from './id';
 
-function addSlide(presentation: Presentation): Presentation {
-    const newSlide: Slide = { // в id нужно сгенерировать значение
+function getEmptySlide(): Slide {
+    return {
         id: generateId(),
         data: [],
         background: {
             type: 'color',
             data: '#ffffff'
         }
-    };
+    }
+}
+
+function addSlide(presentation: Presentation): Presentation {
+    const newSlide: Slide = getEmptySlide();
 
     const selectedSlides: Array<string> | Selection = presentation.selectedSlides;
     let slideIdToInsert: string = Array.isArray(selectedSlides)
@@ -27,6 +31,30 @@ function addSlide(presentation: Presentation): Presentation {
     }
 }
 
+function deleteSlides(presentation: Presentation): Presentation {
+    const selectedSlides: Array<string> | Selection = presentation.selectedSlides;
+
+    const selectedSlidesIdToDelete: Array<string> = Array.isArray(selectedSlides)
+        ? selectedSlides
+        : [selectedSlides.selectedSlideId];
+
+    let newSlides = presentation.slides.filter((slide: Slide) => 
+        selectedSlidesIdToDelete.indexOf(slide.id) === -1
+    );
+
+    newSlides = newSlides.length > 0 ? newSlides : [getEmptySlide()]; 
+
+    return {
+        ...presentation,
+        slides: newSlides,
+        selectedSlides: {
+            selectedSlideId: newSlides[0].id,
+            selectedBlocksId: []
+        }
+    }
+}
+
 export {
-    addSlide
+    addSlide,
+    deleteSlides
 }

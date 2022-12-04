2d95c44dfb32e9757a970df1a5e68add12b01acb
givadi <givadi@mail.ru>
2022-12-04 21:34:03 +0300

    S-6 Отрефакторила функцию добавления слайда, добавила выделение нового слайда

--- src/actions/slides.ts
+++ src/actions/slides.ts
@@ -16,18 +16,22 @@ function addSlide(presentation: Presentation): Presentation {
     const newSlide: Slide = getEmptySlide();
 
     const selectedSlides: Array<string> | Selection = presentation.selectedSlides;
-    let slideIdToInsert: string = Array.isArray(selectedSlides)
+    const selectedSlideId: string = Array.isArray(selectedSlides)
         ? selectedSlides[selectedSlides.length - 1]
         : selectedSlides.selectedSlideId;
 
-    let indexToInsert: number = presentation.slides.findIndex(slide => slide.id === slideIdToInsert) + 1;
+    const indexToInsert: number = presentation.slides.findIndex(slide => slide.id === selectedSlideId) + 1;
 
     let newSlides: Array<Slide> = presentation.slides;
     newSlides.splice(indexToInsert, 0, newSlide);
 
     return {
         ...presentation,
-        slides: newSlides
+        slides: newSlides,
+        selectedSlides: {
+            selectedSlideId: newSlide.id,
+            selectedBlocksId: []
+        }
     }
 }
 

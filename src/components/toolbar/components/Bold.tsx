ba45b7db9aaef5d7677d854cd69ecc837936244e
Aleksandra Andreeva <shu-@list.ru>
2022-12-10 18:03:00 +0300

    S-15 начало работы по селекту блока

--- src/components/toolbar/components/Bold.tsx
+++ src/components/toolbar/components/Bold.tsx
@@ -1,7 +1,18 @@
-import styles from '../../common/barIcon/BarIcon.module.css';
+// import selectedBlock from '../../common/barIcon/BarIcon.module.css';
+import styles from './Bold.module.css';
 import bold from '../../../assets/images/barIcons/bold.svg';
+import {Slide} from '../../../data/types';
+import {Selection} from '../../../data/types';
+import {useState} from 'react';
+import React from 'react';
+
+type selectedProps = {
+  slide: Slide,
+  Selection: Selection
+}
 
 const Bold = () => {
+
   return (
     <div className={styles.wrapper}>
       <img className={styles.icon} src={bold} alt='Bold' />
@@ -10,4 +21,22 @@ const Bold = () => {
   );
 }
 
-export default Bold;
+
+export default function makeBlockSelected (props: selectedProps) {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleClick = event => {
+    setIsActive(current => !current);
+  };
+
+  return(
+    <div className={isActive ? 'selectedBlock' : ''} onClick={handleClick}>
+  </div>
+);
+
+}
+
+
+
+
+// export default Bold;

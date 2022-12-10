ec81f7d94788fad576352e468591a82fbb598fd9
Aleksandra Andreeva <shu-@list.ru>
2022-12-10 19:30:37 +0300

    Сохранение презентации в виде json в txt файле

--- src/components/menubar/components/Save.tsx
+++ src/components/menubar/components/Save.tsx
@@ -1,38 +1,15 @@
 import styles from '../../common/barIcon/BarIcon.module.css';
 import save_as from '../../../assets/images/barIcons/save_as.svg';
-import App from '../../../App';
 import { getData } from '../../../data/testData_v2';
-import { useState, useEffect, useRef } from "react";
-import {Presentation} from "../../../data/types";
 
+const handleSave = () => {
+  const element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(JSON.stringify(getData(), null, 2)));
+  element.setAttribute('download', 'name');
+  element.click();
+};
 
-function Save(App, file) {
-// debugger;
-
-  
-  const finished = (error) =>{
-        if (error){
-          console.error(error);
-          return;
-        }
-      }
-      
-  const name = 'Presentation name'; 
-  const jsonData = JSON.stringify(App);
-  console.log("jsonData: ", jsonData);
-
-  const handleSave = () => {
-    const element = document.createElement('a');
-    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(file));
-    element.setAttribute('download', name);
-    element.style.display = 'none';
-    document.body.appendChild(element);
-    element.click();
-    document.body.removeChild(element);
-    Save(App, file);
-    } ;
-
-
+function Save() {
   return (
     <div className={styles.wrapper}>
       <img className={styles.icon} src={save_as} alt='Save presentation' onClick={handleSave} />

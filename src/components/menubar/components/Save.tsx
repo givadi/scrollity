88614c2ab3c53d56abbbea3f9b12ccd3bf65e71b
Aleksandra Andreeva <shu-@list.ru>
2022-12-10 16:33:42 +0300

    Загрузка работает по клику, грузится пока только пустой объект

--- src/components/menubar/components/Save.tsx
+++ src/components/menubar/components/Save.tsx
@@ -6,59 +6,32 @@ import { useState, useEffect, useRef } from "react";
 import {Presentation} from "../../../data/types";
 
 
-
-// const fs = require('fs');
-
-// const Save = (App, file) =>{
-//   const finished = (error) =>{
-//     if (error){
-//       console.error(error);
-//       return;
-//     }
-//     else 
-//       return (
-//     <div className={styles.wrapper}>
-//       <img className={styles.icon} src={save_as} alt='Save' />
-//     </div>
-//       )
-//   }
-
-//   const jsonData = JSON.stringify(App, null, 2);
-//   fs.writefile(file, jsonData, finished);
-
-//   console.log(App);
-//   console.log(jsonData);
-// }
-
-// Save(App, 'presentation.json');
-
-
-
-
 function Save(App, file) {
+// debugger;
+
+  
   const finished = (error) =>{
         if (error){
           console.error(error);
           return;
         }
       }
+      
   const name = 'Presentation name'; 
-  const jsonData = JSON.stringify(App, null, 2);
-  const element = document.createElement('a');
-  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(file));
-  element.setAttribute('download', name);
-
-  element.style.display = 'none';
-  document.body.appendChild(element);
-  element.click();
-  document.body.removeChild(element);
-  console.log(jsonData);
-
+  const jsonData = JSON.stringify(App);
+  console.log("jsonData: ", jsonData);
 
   const handleSave = () => {
+    const element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(file));
+    element.setAttribute('download', name);
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
     Save(App, file);
-    } 
-  
+    } ;
+
 
   return (
     <div className={styles.wrapper}>

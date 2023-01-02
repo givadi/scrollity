2d23bcf3d579be0e888552b1f3bc1bebaaa2311c
Aleksandra Andreeva <shu-@list.ru>
2023-01-02 17:47:39 +0300

    Изменения в menubar для экспорта в pdf

--- src/components/menubar/Menubar.tsx
+++ src/components/menubar/Menubar.tsx
@@ -1,11 +1,13 @@
 import styles from './Menubar.module.css';
 import Open from './components/Open';
 import Save from './components/Save';
+import ExportToPDF from './components/ExportToPDF';
 import View from './components/View';
 import AddNewSlide from './components/AddNewSlide';
 import DeleteSlide from './components/DeleteSlide';
 import Undo from './components/Undo';
 import Redo from './components/Redo';
+import { getState } from '../../data/testData_v2';
 
 
 const Menubar = () => {
@@ -14,6 +16,7 @@ const Menubar = () => {
       Action
       <Open />
       <Save />
+      <ExportToPDF />
       <View />
       <AddNewSlide />
       <DeleteSlide />
